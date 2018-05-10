a39ae7115da4d0920bec4cd1de4e9461ee63f76d
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-05-10 12:11:26 +0200

    [TRAVIS] Reenable coverage reporting and automatic updates of the documentation
    Change-Id: I723b7177eada90364af8a3f49421a8c712d4b1b5

--- deploy_doc.sh
+++ deploy_doc.sh
@@ -39,15 +39,9 @@ fi
 git add --all doc/*
 git commit -m"Update documentation for commit: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $TRAVIS_BUILD_DIR/deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
+chmod 600 travisci_rsa
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add travisci_rsa
 
 # Push new documentation
 git push ${REPO/https:\/\/github.com\//git@github.com:} $TARGET_BRANCH
