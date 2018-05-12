672756593f2448c487fc96e26f1b240a8ce330ce
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-05-12 08:47:06 +0200

    [TRAVIS] Explicitly enable/disable coverage computation in docker
    Change-Id: I09d675e54a62e81fd7be39f767c024505f275d1e

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -4,15 +4,15 @@ C_COMPILER=$1
 CXX_COMPILER=$2
 GCOV=$3
 COVERALLS_TOKEN=$4
+COMPUTE_COVERAGE=$5
 
 DEPS=/home/deps
 SHARED=/home/shared
 
 mkdir $DEPS && cd $DEPS
 
-apt-get update && apt-get install -y $C_COMPILER $CXX_COMPILER
-export CXX=/usr/bin/$CXX_COMPILER
-export CC=/usr/bin/$C_COMPILER
+export CXX=$(which $CXX_COMPILER)
+export CC=$(which $C_COMPILER)
 
 git clone https://github.com/google/googletest.git && cd googletest 
 mkdir -p build && cd build && cmake .. && cmake --build .
@@ -21,19 +21,18 @@ cp -r ../googlemock/include/gmock /usr/local/include/
 cp googlemock/gtest/lib*.a /usr/local/lib
 
 cd $SHARED
-echo "<$GCOV>"
 git checkout master && git pull
 mkdir -p build && cd build && rm -rf *
-if [ "$GCOV" == "" ]; then
-  cmake -DBuildTest=ON ..
-else
+if [ "$COMPUTE_COVERAGE" == "true" ]; then
   cmake -DBuildTest=ON -DCoverage=ON ..
+else
+  cmake -DBuildTest=ON ..
 fi
 cmake --build .
 cd Test && ctest
 
 
-if [ "$GCOV" != "" ]; then
+if [ "$COMPUTE_COVERAGE" == "true" ]; then
   lcov --gcov-tool $GCOV --capture --no-external --directory .. --base-directory ../../Spacy --output-file coverage.info
   lcov --remove coverage.info '*/Spacy/Adapter/*' -o coverage_without_adapter.info
   coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage_without_adapter.info
