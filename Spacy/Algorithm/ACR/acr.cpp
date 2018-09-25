29fbd22687ed24dc4442f5b3184f2205e7110e27
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-09-25 19:50:20 +0200

    Minor cleanup
    Change-Id: I4e388f12b1e8f5e54c5958f736b0b374d9f9f2d0

--- Spacy/Algorithm/ACR/acr.cpp
+++ Spacy/Algorithm/ACR/acr.cpp
@@ -1,143 +1,152 @@
 #include "acr.hh"
 
-#include <Spacy/operator.hh>
-#include <Spacy/Algorithm/dampingFactor.hh>
+#include <Spacy/Algorithm/CG/cg.hh>
 #include <Spacy/Algorithm/CG/linearSolver.hh>
 #include <Spacy/Algorithm/CompositeStep/quadraticModel.hh>
-#include <Spacy/Algorithm/CG/cg.hh>
 #include <Spacy/Algorithm/Scalar/findGlobalMinimizer.hh>
-#include <Spacy/Algorithm/CG/linearSolver.hh>
-#include <Spacy/zeroVectorCreator.hh>
+#include <Spacy/Algorithm/dampingFactor.hh>
 #include <Spacy/Util/cast.hh>
 #include <Spacy/Util/log.hh>
+#include <Spacy/operator.hh>
+#include <Spacy/zeroVectorCreator.hh>
 
 #include <cmath>
-#include <iostream>
 #include <utility>
-
 
 namespace
 {
     class TrivialPreconditioner
     {
     public:
-        //compute A(x)
-        Spacy::Vector operator()(Spacy::Vector x) const{ return x; }
+        // compute A(x)
+        const Spacy::Vector& operator()( const Spacy::Vector& x ) const
+        {
+            return x;
+        }
     };
 }
-
 
 namespace Spacy
 {
     namespace ACR
     {
         DEFINE_LOG_TAG( static const char* log_tag = "ACR" );
-         
-        CompositeStep::CubicModel makeCubicModel(const Vector& dx,
-                                                 const C2Functional& f, const Vector& x, Spacy::Real omega)
+
+        CompositeStep::CubicModel makeCubicModel( const Vector& dx, const C2Functional& f,
+                                                  const Vector& x, Spacy::Real omega )
         {
-            return CompositeStep::CubicModel( CompositeStep::makeQuadraticModel(Spacy::DampingFactor(0.0),dx,dx,f,x), CompositeStep::makeQuadraticNormModel(Spacy::DampingFactor(0.0),dx,dx), omega );
+            return CompositeStep::CubicModel(
+                CompositeStep::makeQuadraticModel( Spacy::DampingFactor( 0.0 ), dx, dx, f, x ),
+                CompositeStep::makeQuadraticNormModel( Spacy::DampingFactor( 0.0 ), dx, dx ),
+                omega );
         }
 
-        ACRSolver::ACRSolver(C2Functional f,   double eta1,  double eta2, double epsilon, double relativeAccuracy,  double omegaMax, double lambdaMax)
-            : Mixin::RelativeAccuracy(relativeAccuracy),
-              f_(std::move(f)),
-              domain_(f_.domain()),
-              eta1_(eta1), eta2_(eta2),  epsilon_(epsilon),  omegaMax_(omegaMax),  lambdaMax_(lambdaMax)
-        {}
+        ACRSolver::ACRSolver( C2Functional f, double eta1, double eta2, double epsilon,
+                              double relativeAccuracy, double omegaMax, double lambdaMax )
+            : Mixin::RelativeAccuracy( relativeAccuracy ), f_( std::move( f ) ),
+              domain_( f_.domain() ), eta1_( eta1 ), eta2_( eta2 ), epsilon_( epsilon ),
+              omegaMax_( omegaMax ), lambdaMax_( lambdaMax )
+        {
+        }
 
         Vector ACRSolver::operator()()
         {
-            return operator()( zero(domain_) );
+            return operator()( zero( domain_ ) );
         }
 
-        Vector ACRSolver::operator()(const Vector& x0)
-        {  
-            LOG_SEPARATOR(log_tag);
+        Vector ACRSolver::operator()( const Vector& x0 )
+        {
+            LOG_SEPARATOR( log_tag );
 
             auto x = x0;
 
-            for(unsigned step = 1; step <= getMaxSteps(); ++step)
+            for ( unsigned step = 1; step <= getMaxSteps(); ++step )
             {
-                
-                LOG(log_tag, "Iteration", step)
-                
+
+                LOG( log_tag, "Iteration", step )
+
                 stepMonitor = StepMonitor::Accepted;
                 // TODO domain_.setScalarProduct( );
                 Real lambda = 1.0;
-        
+
                 do
                 {
-                    auto dx = computeStep(x);
-                    LOG(log_tag, "|dx|",  norm(dx) );
-                    auto cubicModel = makeCubicModel(dx,f_,x,omega_);
-                    LOG_INFO(log_tag, "Computing damping factor")
+                    auto dx = computeStep( x );
+                    LOG( log_tag, "|dx|", norm( dx ) );
+                    auto cubicModel = makeCubicModel( dx, f_, x, omega_ );
+                    LOG_INFO( log_tag, "Computing damping factor" )
 
-                    lambda = Scalar::findGlobalMinimizer(cubicModel, 0, 1, 1e-2);
+                    lambda = Scalar::findGlobalMinimizer( cubicModel, 0, 1, 1e-2 );
 
-                    dx = lambda*dx;
+                    dx = lambda * dx;
 
-                    LOG(log_tag, "lambda: ", lambda, " omega: ",  omega_, " cubicModel: ", cubicModel(lambda))
+                    LOG( log_tag, "lambda: ", lambda, " omega: ", omega_, " cubicModel: ",
+                         cubicModel( lambda ) )
 
-
-                    if( (stepMonitor = acceptanceTest(x,dx,lambda,cubicModel)) == StepMonitor::Accepted )
+                    if ( ( stepMonitor = acceptanceTest( x, dx, lambda, cubicModel ) ) ==
+                         StepMonitor::Accepted )
                     {
                         x += dx;
                         //  if( convergenceTest(/*TODO*/) ) return x;
                         TrivialPreconditioner trivialP;
                         // Todo: Change Norm
-                        if (f_.d1(x)(trivialP(f_.d1(x))) < epsilon_ * epsilon_) {
-                            LOG_INFO(log_tag, "Converged")
+                        if ( f_.d1( x )( trivialP( f_.d1( x ) ) ) < epsilon_ * epsilon_ )
+                        {
+                            LOG_INFO( log_tag, "Converged" )
                             return x;
                         }
                     }
 
-                    else LOG_INFO(log_tag, "Rejected")
+                    else
+                        LOG_INFO( log_tag, "Rejected" )
 
-                    // Modifikation von omega
-                    omega_ = weightChange(omega_);
+                    omega_ = weightChange( omega_ );
 
                     if ( omega_ > omegaMax_ )
                         return x;
                 }
                 // end iteration
-                while( stepMonitor == StepMonitor::Rejected || lambda < lambdaMax_ );
-                LOG_SEPARATOR(log_tag);
+                while ( stepMonitor == StepMonitor::Rejected || lambda < lambdaMax_ );
+                LOG_SEPARATOR( log_tag );
             }
 
-            LOG_INFO(log_tag, "Reached maximum number of steps");
+            LOG_INFO( log_tag, "Reached maximum number of steps" );
             return x;
         }
 
-        ACRSolver::StepMonitor ACRSolver::acceptanceTest(const Vector &x, const Vector &dx, const Real & lambda, const CompositeStep::CubicModel& cubicModel)
+        ACRSolver::StepMonitor
+        ACRSolver::acceptanceTest( const Vector& x, const Vector& dx, const Real& lambda,
+                                   const CompositeStep::CubicModel& cubicModel )
         {
-            
-            // Prevent dividing by zero e.g. in the case x_0 = opimal solution
-            // Consider the Case numerator = nominator = 0
-            
-            auto diff = cubicModel(lambda)-cubicModel(0.0);
-                
-            if (abs(diff) < eps() ) 
-            {
-                if ( diff < 0) diff = -eps();
-                     
-                else  diff = eps();
-            }
-            
-            rho_ = (f_(x+dx)-f_(x))/diff;
+            auto diff = cubicModel( lambda ) - cubicModel( 0.0 );
 
-            LOG(log_tag, "f_(x+dx): ",  f_(x+dx),  "f_(x): ",  f_(x), "CubicModel(lambda): ",  cubicModel(lambda), "CubicModel(0): ",  cubicModel(0), "rho: ", rho_,  "eta1 ",  eta1_,  "eta2 ",  eta2_ )
-         
-            if (rho_ >= eta1_)
+            // Prevent dividing by zero e.g. in the case x_0 = optimal solution
+            // Consider the Case numerator = nominator = 0
+            if ( abs( diff ) < eps() )
+            {
+                if ( diff < 0 )
+                    diff = -eps();
+
+                else
+                    diff = eps();
+            }
+
+            rho_ = ( f_( x + dx ) - f_( x ) ) / diff;
+
+            LOG( log_tag, "f_(x+dx): ", f_( x + dx ), "f_(x): ", f_( x ), "CubicModel(lambda): ",
+                 cubicModel( lambda ), "CubicModel(0): ", cubicModel( 0 ), "rho: ", rho_, "eta1 ",
+                 eta1_, "eta2 ", eta2_ )
+
+            if ( rho_ >= eta1_ )
                 return StepMonitor::Accepted;
 
             return StepMonitor::Rejected;
         }
 
-        Spacy::Real ACRSolver::weightChange(Spacy::Real omega) const
+        Spacy::Real ACRSolver::weightChange( Spacy::Real omega ) const
         {
-            
-            LOG(log_tag, "rho: ",  rho_,  "eta1 ",  eta1_,  "eta2 ",  eta2_  )
+
+            LOG( log_tag, "rho: ", rho_, "eta1 ", eta1_, "eta2 ", eta2_ )
 
             if ( rho_ > eta2_ )
                 omega *= 0.5;
@@ -147,20 +156,14 @@ namespace Spacy
             return omega;
         }
 
-        Vector ACRSolver::computeStep(const Spacy::Vector &x) const
+        Vector ACRSolver::computeStep( const Spacy::Vector& x ) const
         {
-//            LinearSolver preconditioner = {};
-//            preconditioner = f_.hessian(x)^-1;
-                        
-//            auto & cgSolver = cast_ref<CG::LinearSolver>(preconditioner);
-            auto tcg =  makeTCGSolver( f_.hessian(x) , [](const Vector& x){ return x; });
-//
-//             auto tcg =  makeTCGSolver( f_.hessian(x) , f_.hessian(x).solver());
-            tcg.setRelativeAccuracy(getRelativeAccuracy());
+            auto tcg = makeTCGSolver( f_.hessian( x ), []( const Vector& x ) { return x; } );
+            tcg.setRelativeAccuracy( getRelativeAccuracy() );
 
-            LOG(log_tag, "rhs", norm(f_.d1(x)));
+            LOG( log_tag, "rhs", norm( f_.d1( x ) ) );
 
-            return tcg(-f_.d1(x));
+            return tcg( -f_.d1( x ) );
         }
     }
 }
--- Spacy/Algorithm/ACR/acr.hh
+++ Spacy/Algorithm/ACR/acr.hh
@@ -1,43 +1,50 @@
 #pragma once
 
-#include <chrono>
-
-#include <Spacy/linearSolver.hh>
+#include <Spacy/Spaces/ScalarSpace/Real.hh>
+#include <Spacy/Util/mixins.hh>
 #include <Spacy/c2Functional.hh>
+#include <Spacy/linearSolver.hh>
 #include <Spacy/vector.hh>
 #include <Spacy/vectorSpace.hh>
-#include <Spacy/Util/mixins.hh>
-#include <Spacy/Spaces/ScalarSpace/Real.hh>
 
 namespace Spacy
 {
     /// @cond
-    namespace CompositeStep { class CubicModel; }
+    namespace CompositeStep
+    {
+        class CubicModel;
+    }
     /// @endcond
 
     namespace ACR
     {
         /**
-         * @brief Adaptive cubic regularization approach for solving unconstrained minimization problems
+         * @brief Adaptive cubic regularization approach for solving unconstrained minimization
+         * problems
          * Compute direction of descent with one Newton step
-         * Use a cubic error model to obtain acceptable corrections 
+         * Use a cubic error model to obtain acceptable corrections
          * Solve problems of the form
          * \f[\min f(x).\f]
          */
-        class ACRSolver :
-                public Mixin::Eps,
-                public Mixin::MaxSteps,
-                public Mixin::RelativeAccuracy,
-                public Mixin::Verbosity
+        class ACRSolver : public Mixin::Eps,
+                          public Mixin::MaxSteps,
+                          public Mixin::RelativeAccuracy,
+                          public Mixin::Verbosity
         {
-            enum class StepMonitor { Rejected , Accepted };
+            enum class StepMonitor
+            {
+                Rejected,
+                Accepted
+            };
 
         public:
             /**
              * @brief Constructor.
              * @param f functional to minimize
              */
-            ACRSolver(C2Functional f, double eta1 = 0.25, double eta2 = 0.5, double epsilon = 1e-4, double relativeAccuracy = 1e-4, double omegaMax = 1e8,  double lambdaMax = 2e-2);
+            ACRSolver( C2Functional f, double eta1 = 0.25, double eta2 = 0.5, double epsilon = 1e-4,
+                       double relativeAccuracy = 1e-4, double omegaMax = 1e8,
+                       double lambdaMax = 2e-2 );
 
             /// Compute solution starting at \f$x_0=0\f$.
             Vector operator()();
@@ -46,15 +53,14 @@ namespace Spacy
              * @brief Compute solution.
              * @param x0 initial iterate
              */
-            Vector operator()(const Vector& x0);
+            Vector operator()( const Vector& x0 );
 
         private:
-            
             /**
              * @brief Compute correction dx.
              * @param x current iterate
              */
-            Vector computeStep(const Vector& x) const;
+            Vector computeStep( const Vector& x ) const;
 
             /**
              * @brief Test if dx is an acceptable correction.
@@ -63,23 +69,23 @@ namespace Spacy
              * @param lambda damping factor
              * @param cubicModel cubic model
              */
-            StepMonitor acceptanceTest(const Vector& x, const Vector& dx, const Real& lambda, const CompositeStep::CubicModel& cubicModel);
+            StepMonitor acceptanceTest( const Vector& x, const Vector& dx, const Real& lambda,
+                                        const CompositeStep::CubicModel& cubicModel );
 
             /**
              * @brief Update the weight Parameter of the cubic model.
              * @param omega weight Parameter of the cubic model
              */
-            Real weightChange(::Spacy::Real omega) const;
+            Real weightChange(::Spacy::Real omega ) const;
 
             C2Functional f_;
             const VectorSpace& domain_;
             StepMonitor stepMonitor = StepMonitor::Accepted;
-            
+
             // epsilon_ termination criterion
-            const double eta1_, eta2_, epsilon_, omegaMax_,  lambdaMax_;
+            const double eta1_, eta2_, epsilon_, omegaMax_, lambdaMax_;
             Real rho_ = 1;
             Real omega_ = 1e-6;
         };
     }
 }
-
