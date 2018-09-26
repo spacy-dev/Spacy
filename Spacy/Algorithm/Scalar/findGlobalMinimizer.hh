68348222f79e20414952850b2cd0009b1d29b4fe
Manuel Schaller <manuelschaller@gmx.de>
2018-09-26 09:30:03 +0200

    Added a Brent linesearch
    Change-Id: I44040adbed3ad85f287240dde023632c9d733be6

--- Spacy/Algorithm/Scalar/findGlobalMinimizer.hh
+++ Spacy/Algorithm/Scalar/findGlobalMinimizer.hh
@@ -3,6 +3,7 @@
 #include <functional>
 #include <Spacy/Spaces/ScalarSpace/Real.hh>
 
+
 namespace Spacy
 {
     namespace Scalar
@@ -45,6 +46,16 @@ namespace Spacy
 
         Real findLogGlobalMinimizerTangentialDamping(const std::function<Real(const Real)>& f, Real a, Real b, const Real normdn2,
                                                      const Real two_sp_dn_Dt, const Real normDt2, Real eps = 1e-2);
+
+        /**
+         * @brief Find global minimizer of \f$f\f$ in \f$[a,b]\f$ with a Brent line search
+         * @param f Nonlinear function \f$ f: \mathbb{R}\to\mathbb{R} \f$
+         * @param a lower bound of the admissible interval
+         * @param b upper bound of the admissible interval
+         * @param eps relative accuracy
+         * @return \f$ x \in \mathrm{argmin}_{[a,b]} \f$
+         */
+        Real findMinBrent(const std::function<Real(const Real)>& f, Real a, Real b, Real eps = 3.e-8);
 
     }
 }
