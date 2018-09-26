128cbd155c929f64c5fcdf2ce546a963fc75ee6e
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-09-26 20:24:38 +0200

    Remove unused class AdaptiveInducedScalarProduct
    Change-Id: I4ffd62a20ec15b77cae5ad70d394f9c85927041e

--- Spacy/inducedScalarProduct.cpp
+++ Spacy/inducedScalarProduct.cpp
@@ -1,50 +1,33 @@
 #include "inducedScalarProduct.hh"
 
-#include <Spacy/Util/cast.hh>
 #include <Spacy/Spaces/ProductSpace/vector.hh>
+#include <Spacy/Util/cast.hh>
 
 #include <utility>
 
 namespace Spacy
 {
-  AdaptiveInducedScalarProduct::AdaptiveInducedScalarProduct(std::function<LinearOperator()> operatorCreator)
-    : operatorCreator_(std::move(operatorCreator))
-  {}
+    InducedScalarProduct::InducedScalarProduct( LinearOperator M ) : M_( std::move( M ) )
+    {
+    }
 
-  Real AdaptiveInducedScalarProduct::operator()(const Vector& x, const Vector& y) const
-  {
-    return operatorCreator_()(y)(x);
-  }
+    Real InducedScalarProduct::operator()( const Vector& x, const Vector& y ) const
+    {
+        return M_( y )( x );
+    }
 
+    PrimalInducedScalarProduct::PrimalInducedScalarProduct( LinearOperator M )
+        : M_( std::move( M ) )
+    {
+    }
 
-  InducedScalarProduct::InducedScalarProduct(LinearOperator M)
-    : M_(std::move(M))
-  {}
+    Real PrimalInducedScalarProduct::operator()( Vector x, Vector y ) const
+    {
+        auto& x_ = cast_ref< ProductSpace::Vector >( x );
+        auto& y_ = cast_ref< ProductSpace::Vector >( y );
+        x_.component( DUAL ) *= 0;
+        y_.component( DUAL ) *= 0;
 
-  Real InducedScalarProduct::operator()(const Vector& x, const Vector& y) const
-  {
-    return M_(y)(x);
-  }
-
-
-  PrimalInducedScalarProduct::PrimalInducedScalarProduct(LinearOperator M)
-    : M_(std::move(M))
-  {}
-
-  Real PrimalInducedScalarProduct::operator()(const Vector& x, const Vector& y) const
-  {
-//    std::cout << "induced scalar product" << std::endl;
-//    auto& x_ = cast_ref<ProductSpace::Vector>(x);
-//    auto& y_ = cast_ref<ProductSpace::Vector>(y);
-//    x_.component(DUAL) *= 0;
-//    y_.component(DUAL) *= 0;
-
-//    /* TODO PROJECTION */
-//    std::cout << "PISP y: " << norm(y) << std::endl;
-//    std::cout << "PISP My: " << norm(M_(y)) << std::endl;
-//    std::cout << "PISP x: " << norm(x) << std::endl;
-//    std::cout << "PISP xMy: " << M_(y)(x) << std::endl;
-
-    return M_(y)(x);
-  }
+        return M_( y )( x );
+    }
 }
--- Spacy/inducedScalarProduct.hh
+++ Spacy/inducedScalarProduct.hh
@@ -6,63 +6,46 @@
 
 namespace Spacy
 {
-  /// Induced scalar product \f$(x,y)_M = (Mx)y\f$, where \f$M:X\rightarrow X^*\f$.
-  class AdaptiveInducedScalarProduct
-  {
-  public:
+    /// Induced scalar product \f$(x,y)_M = (Mx)y\f$, where \f$M:X\rightarrow X^*\f$.
+    class InducedScalarProduct
+    {
+    public:
+        /**
+         * @brief Constructor.
+         * @param M operator defining the scalar product.
+         */
+        explicit InducedScalarProduct( LinearOperator M );
+
+        /// Compute scalar product \f$(x,y)_M\f$.
+        Real operator()( const Vector& x, const Vector& y ) const;
+
+    private:
+        LinearOperator M_;
+    };
+
     /**
-     * @brief Constructor.
-     * @param M operator defining the scalar product.
+     * @brief Induced scalar product for the primal variables (i.e. for constrained optimization
+     * problems).
+     *
+     * Let
+     * \f$ M = \left( \begin{array}{ccc} A & B^T \\ B &  \end{array} \right)\f$.
+     * Then the primal induced scalar product is
+     * \f[ (z_1,z_2)_M = (Ax_1)(x_2) + (p_1,p_2)_Q  \f]
+     * for \f$z_i = (x_i,p_i)\in X\times Q,\ i=1,2\f$.
      */
-    explicit AdaptiveInducedScalarProduct(std::function<LinearOperator()> operatorCreator);
+    class PrimalInducedScalarProduct
+    {
+    public:
+        /**
+         * @brief Constructor.
+         * @param M linear operator defining the scalar product.
+         */
+        explicit PrimalInducedScalarProduct( LinearOperator M );
 
-    /// Compute scalar product \f$(x,y)_M\f$.
-    Real operator()(const Vector& x, const Vector& y) const;
+        /// @brief Compute scalar product \f$(x,y)_M\f$.
+        Real operator()( Vector x, Vector y ) const;
 
-  private:
-//    LinearOperator M_;
-    std::function<LinearOperator()> operatorCreator_;
-  };
-
-  /// Induced scalar product \f$(x,y)_M = (Mx)y\f$, where \f$M:X\rightarrow X^*\f$.
-  class InducedScalarProduct
-  {
-  public:
-    /**
-     * @brief Constructor.
-     * @param M operator defining the scalar product.
-     */
-    explicit InducedScalarProduct(LinearOperator M);
-
-    /// Compute scalar product \f$(x,y)_M\f$.
-    Real operator()(const Vector& x, const Vector& y) const;
-
-  private:
-    LinearOperator M_;
-  };
-
-  /**
-   * @brief Induced scalar product for the primal variables (i.e. for constrained optimization problems).
-   *
-   * Let
-   * \f$ M = \left( \begin{array}{ccc} A & B^T \\ B &  \end{array} \right)\f$.
-   * Then the primal induced scalar product is
-   * \f[ (z_1,z_2)_M = (Ax_1)(x_2) + (p_1,p_2)_Q  \f]
-   * for \f$z_i = (x_i,p_i)\in X\times Q,\ i=1,2\f$.
-   */
-  class PrimalInducedScalarProduct
-  {
-  public:
-    /**
-     * @brief Constructor.
-     * @param M linear operator defining the scalar product.
-     */
-    explicit PrimalInducedScalarProduct(LinearOperator M);
-
-    /// @brief Compute scalar product \f$(x,y)_M\f$.
-    Real operator()(const Vector& x, const Vector& y) const;
-
-  private:
-    LinearOperator M_;
-  };
+    private:
+        LinearOperator M_;
+    };
 }
