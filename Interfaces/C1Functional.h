b8412be0665e7a502fb037e7f84217e023192067
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-11-14 23:29:31 +0100

    Rename interface files
    Change-Id: I19a27b77973116b9652f5320a224f8160fa3efff

---
+++ Interfaces/C1Functional.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Spacy/Spaces/ScalarSpace/Real.h>
+#include <Spacy/vector.hh>
+#include <Spacy/vectorSpace.hh>
+
+namespace Spacy
+{
+    /// Type-erased differentiable functional \f$f:\ X \to \mathbb{R} \f$.
+    class C1Functional
+    {
+    public:
+        /// Apply functional.
+        Real operator()( const Vector& x ) const;
+
+        /// Compute derivative as function space element in \f$X^*\f$, where \f$x\in X\f$.
+        Vector d1( const Vector& x ) const;
+
+        /// Access domain space \f$X\f$.
+        const VectorSpace& domain() const;
+    };
+}
---
+++ Interfaces/LinearSolver.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Spacy/vector.hh>
+#include <functional>
+
+namespace Spacy
+{
+    /// Type-erased linear solver.
+    using LinearSolver = std::function< Vector( const Vector& ) >;
+
+    /// Type-erased indefinite linear solver. Additionally monitors if the underlying operator is
+    /// positive definite.
+    class IndefiniteLinearSolver
+    {
+    public:
+        Vector operator()( const Vector& x ) const;
+
+        bool isPositiveDefinite() const;
+    };
+}
