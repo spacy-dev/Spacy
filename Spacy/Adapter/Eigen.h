71f6507b58540c69c83571126c54470441ee50dd
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-11-14 21:07:45 +0100

    Rename file
    Change-Id: If703a4e7eacf67fb527478e2e49f5e2fcbd7b8cc

---
+++ Spacy/Adapter/Eigen.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define EIGEN_INITIALIZE_MATRICES_BY_ZERO
+
+#include "Eigen/C1Operator.h"
+#include "Eigen/C2Functional.h"
+#include "Eigen/LinearOperator.h"
+#include "Eigen/LinearOperatorCreator.h"
+#include "Eigen/LinearSolver.h"
+#include "Eigen/ScalarProduct.h"
+#include "Eigen/Vector.h"
+#include "Eigen/vectorCreator.h"
