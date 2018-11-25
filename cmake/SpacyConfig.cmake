e2649a80c82e5b1625e046cb6972660ade9e6c5e
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-11-25 16:14:42 +0100

    [CMake] Introduce target Spacy::Spacy
    Change-Id: Ib58b29e5ea0cfa8a3ac7e86c97f3ef3858e62309

--- cmake/SpacyConfig.cmake
+++
@@ -1,8 +0,0 @@
-# - Config file for the Spacy package 
-set(Spacy_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
-set(Spacy_LIBRARY_DIR ${CMAKE_INSTALL_PREFIX}/lib)
-set(Spacy_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/libspacy.a)
-set(Spacy_LIBS spacy)
-set(Spacy_CXX_FLAGS "-std=c++1y")
-set(Spacy_CXX_DEFINITIONS "-std=c++1y")
-set(Spacy_CXX_STANDARD 14)
