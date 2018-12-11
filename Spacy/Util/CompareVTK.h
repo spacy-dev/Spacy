d5bb39facf17226cfa1c5db9cf02ecb97c46525a
Lars Lubkoll <lars.lubkoll@posteo.de>
2018-12-11 21:22:53 +0100

    Return false if opening of vtk-files fails during comparison
    Change-Id: I55df87917e04449623fea41a477d12e3fddf18cc

--- Spacy/Util/CompareVTK.h
+++ Spacy/Util/CompareVTK.h
@@ -2,55 +2,57 @@
 
 #include <algorithm>
 #include <cmath>
+#include <string>
+#include <vector>
 #include <fstream>
 #include <iterator>
 #include <sstream>
-#include <string>
-#include <vector>
 
 namespace Spacy
 {
-    std::vector<double> lookupTableToVector(const std::string& line)
+    std::vector< double > lookupTableToVector( const std::string& line )
     {
-        auto iss = std::istringstream(line);
-        std::vector<std::string> tokens{std::istream_iterator<std::string>(iss),
-                                        std::istream_iterator<std::string>()};
-        std::vector<double> values;
-        std::transform(begin(tokens), end(tokens),
-                       std::back_inserter(values),
-                       [](const std::string& token)
-        {
-            double value = std::stod(token);
-            return value;
-        });
+        auto iss = std::istringstream( line );
+        std::vector< std::string > tokens{std::istream_iterator< std::string >( iss ),
+                                          std::istream_iterator< std::string >()};
+        std::vector< double > values;
+        std::transform( begin( tokens ), end( tokens ), std::back_inserter( values ),
+                        []( const std::string& token ) {
+                            double value = std::stod( token );
+                            return value;
+                        } );
         return values;
     }
 
-    std::vector<double> extractLookupTable(const std::string& filename)
+    std::vector< double > extractLookupTable( std::ifstream& file )
     {
-        auto file = std::ifstream(filename);
         auto foundLookupTable = false;
-        while(!file.eof())
+        while ( !file.eof() )
         {
             std::string line;
-            std::getline(file, line);
-            if(foundLookupTable)
-                return lookupTableToVector(line);
-            foundLookupTable = line.compare(0, 12, "LOOKUP_TABLE") == 0;
+            std::getline( file, line );
+            if ( foundLookupTable )
+                return lookupTableToVector( line );
+            foundLookupTable = line.compare( 0, 12, "LOOKUP_TABLE" ) == 0;
         }
         return {};
     }
 
-    bool compareLookupTable(const std::string& lhsVTKFileName, const std::string& rhsVTKFileName)
+    bool compareLookupTable( const std::string& lhsVTKFileName, const std::string& rhsVTKFileName )
     {
-        const auto lhsValues = extractLookupTable(lhsVTKFileName);
-        const auto rhsValues = extractLookupTable(rhsVTKFileName);
+        auto lhsFile = std::ifstream( lhsVTKFileName );
+        auto rhsFile = std::ifstream( rhsVTKFileName );
+        if ( !lhsFile.is_open() !!!rhsFile.is_open() )
+            return false;
+        const auto lhsValues = extractLookupTable( lhsFile );
+        const auto rhsValues = extractLookupTable( rhsFile );
 
-        if(lhsValues.size() != rhsValues.size())
+        if ( lhsValues.size() != rhsValues.size() )
             return false;
 
-        for(auto i=0u; i<lhsValues.size(); ++i)
-            if(std::abs(lhsValues[i] - rhsValues[i]) > std::numeric_limits<double>::epsilon())
+        for ( auto i = 0u; i < lhsValues.size(); ++i )
+            if ( std::abs( lhsValues[ i ] - rhsValues[ i ] ) >
+                 std::numeric_limits< double >::epsilon() )
                 return false;
         return true;
     }
