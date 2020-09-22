6d528dfb77cce98857666c999063fcb58bfeb777
Anton Schiela <anton.schiela@uni-bayreuth.de>
2020-09-22 18:52:42 +0200

    VTKWriter auskommentiert, compiliert sonst nicht

--- Spacy/Adapter/Kaskade/WriteVTK.h
+++ Spacy/Adapter/Kaskade/WriteVTK.h
@@ -10,18 +10,18 @@ namespace Spacy
 {
     namespace Kaskade
     {
-        template < class Description >
-        void writeVTK( const Vector< Description >& x, const std::string& fileName )
-        {
-            typename Description::VariableSet y( *x.description_ );
-            copy( x, y );
-            ::Kaskade::writeVTKFile( boost::fusion::at_c< 0 >( x.description_->spaces )
-                                         ->gridManager()
-                                         .grid()
-                                         .leafGridView(),
-                                     y, fileName, ::Kaskade::IoOptions{},
-                                     1 ); // at_c<0>(spaces_)->mapper().getOrder());
-        }
+//         template < class Description >
+//         void writeVTK( const Vector< Description >& x, const std::string& fileName )
+//         {
+//             typename Description::VariableSet y( *x.description_ );
+//             copy( x, y );
+//             ::Kaskade::writeVTKFile( boost::fusion::at_c< 0 >( x.description_->spaces )
+//                                          ->gridManager()
+//                                          .grid()
+//                                          .leafGridView(),
+//                                      y, fileName, ::Kaskade::IoOptions{},
+//                                      1 ); // at_c<0>(spaces_)->mapper().getOrder());
+//         }
 
         //    template <class Description>
         //    void writeVTK(const ::Spacy::Vector& x, const std::string& fileName)
