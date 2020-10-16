1bc5b641470dd1c9387471992e5451cb6cc73e0a
Anton Schiela <anton.schiela@uni-bayreuth.de>
2020-10-16 13:11:38 +0200

    Added Embeddings and Proejctions

---
+++ Spacy/Vector.cpp
@@ -0,0 +1,57 @@
+#include "Vector.h"
+
+#include <Spacy/Util/Cast.h>
+#include <Spacy/Util/Exceptions.h>
+
+#include <Spacy/HilbertSpaceNorm.h>
+#include <Spacy/Spaces/ScalarSpace/Real.h>
+#include <Spacy/ZeroVectorCreator.h>
+
+#include <stdexcept>
+#include <utility>
+#include <iostream>
+
+namespace Spacy
+{
+     Vector& Vector::operator=( const Vector& v )
+        {
+            if ( impl_ )
+            {
+                checkSpaceCompatibility(v.space(),this->space());
+                globalSpaceManager().unsubscribe( this );
+                if(v.space().index() != this->space().index())
+                {
+                    v.space().getEmbedding(this->space()).apply(v,*this);
+                } else
+                {
+                   impl_=v.impl_;
+                }
+             } 
+             else
+             {
+                impl_ = v.impl_;
+             }
+             if ( impl_ )
+                 globalSpaceManager().subscribe( this );
+             return *this;
+        }
+
+       Vector& Vector::operator=( Vector&& v )
+        {
+            if ( impl_ )
+            {
+                checkSpaceCompatibility(v.space(),this->space());
+                globalSpaceManager().unsubscribe( this );
+                if(v.space().index() != this->space().index())
+                {
+                   v.space().getEmbedding(this->space()).apply(v,*this);
+                }
+            }
+            impl_ = std::move( v ).impl_;
+            if ( impl_ )
+                globalSpaceManager().subscribe( this );
+            return *this;
+        }
+
+
+}
