26a743b0dd04c0e18d2c1423dcffd94a9e1835f3
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-11-08 13:09:32 +0100

    Add cmake-variants.json
    Change-Id: I0963fc17124b0aa4d391e29ef442c621c3587fc3

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
-mkdir -p "$REPO_ROOT"/build
-cd "$REPO_ROOT"/build || exit 1
+mkdir -p "$REPO_ROOT"/build-Release
+cd "$REPO_ROOT"/build-Release || exit 1
 conan install .. --build=missing
