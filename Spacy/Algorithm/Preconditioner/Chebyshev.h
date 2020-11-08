f21855f01dfbbb4ab627b837500e0be6e8a64ab3
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-11-08 19:32:26 +0100

    Fix documentation
    Change-Id: I3420b8c9520d98daa83743eb5994c494058ee986

--- Spacy/Algorithm/Preconditioner/Chebyshev.h
+++ Spacy/Algorithm/Preconditioner/Chebyshev.h
@@ -3,65 +3,53 @@
 #include <tuple>
 
 #include <Spacy/Operator.h>
-#include <Spacy/Util/Base/OperatorBase.h>
+#include <Spacy/Spaces/ScalarSpace/Real.h>
 #include <Spacy/Util/Mixins.h>
-#include <Spacy/Util/Mixins/Get.h>
+#include <Spacy/Vector.h>
 
-namespace Spacy
+namespace Spacy::Preconditioner
 {
-    /// \cond
-    class VectorSpace;
-    /// \endcond
+    /**
+     *  @brief An auxilliary function, needed by Chebyshev: from the coefficients, used by cg, bounds on the spectrum of the
+     * corresponding operator can be computed via a Lanczos-Matrix
+     *
+     *  @return: (lower spectral bound, upper spectral bound)
+     */
+    std::tuple< double, double > computeEigsFromCGCoefficients( const std::vector< Spacy::Real >& alpha,
+                                                                const std::vector< Spacy::Real >& beta );
 
-    namespace Preconditioner
+    /**
+     * @brief A Preconditioner based on the chebyshev-iteration solving A*x=b
+     */
+    class Chebyshev : public Mixin::Eps,
+                      public Mixin::MaxSteps,
+                      public Mixin::Verbosity,
+                      public Mixin::AbsoluteAccuracy,
+                      public Mixin::RelativeAccuracy
     {
+    public:
         /**
-         *  @brief An auxilliary function, needed by Chebyshev: from the coefficients, used by cg, bounds on the spectrum of the
-         * corresponding operator can be computed via a Lanczos-Matrix
-         *
-         *  @param eigMin: lower spectral bound
-         *  @param eigMax: upper spectral bound
+         * @brief Constructor.
+         * @param A linear Operator
+         * @param P preconditioner
+         * @param gamma_max largest eigenvalue of A
+         * @param gamma_min smallest eigenvalue of A
          */
-        std::tuple< double, double > computeEigsFromCGCoefficients( const std::vector< Spacy::Real >& alpha,
-                                                                    const std::vector< Spacy::Real >& beta );
-
+        Chebyshev( ::Spacy::CallableOperator A, ::Spacy::CallableOperator P, ::Spacy::Real gamma_max = 1.0, ::Spacy::Real gamma_min = 1.0 );
         /**
-         * @brief A Preconditioner based on the chebyshev-iteration solving A*x=b
+         * @brief Apply preconditioner.
+         * @param b argument
          */
-        class Chebyshev : public Mixin::Eps,
-                          public Mixin::MaxSteps,
-                          public Mixin::Verbosity,
-                          public Mixin::AbsoluteAccuracy,
-                          public Mixin::RelativeAccuracy
-        {
-        public:
-            /**
-             * @brief Constructor.
-             * @param A linear Operator
-             * @param P preconditioner
-             * @param gamma_max largest eigenvalue of A
-             * @param gamma_min smallest eigenvalue of A
-             */
-            Chebyshev( ::Spacy::CallableOperator A, ::Spacy::CallableOperator P, ::Spacy::Real gamma_max = 1.0,
-                       ::Spacy::Real gamma_min = 1.0 );
-            /**
-             * @brief Apply preconditioner.
-             * @param b argument
-             */
-            Vector operator()( const Vector& b ) const;
+        Vector operator()( const Vector& b ) const;
 
-            int getIterations() const;
+        int getIterations() const;
 
-            void setSpectralBounds( Real gamma_min, Real gamma_max );
+        void setSpectralBounds( Real gamma_min, Real gamma_max );
 
-        private:
-            ::Spacy::CallableOperator A_;
-            ::Spacy::CallableOperator P_;
-            ::Spacy::Real gamma_max_{ 1.0 };
-            ::Spacy::Real gamma_min_{ 1.0 };
-
-            std::function< ::Spacy::Vector( const ::Spacy::Vector&, const ::Spacy::Vector& ) > transfer_ =
-                []( const ::Spacy::Vector& src, const ::Spacy::Vector& res ) { return src; };
-        };
-    } // namespace Preconditioner
-} // namespace Spacy
+    private:
+        ::Spacy::CallableOperator A_;
+        ::Spacy::CallableOperator P_;
+        ::Spacy::Real gamma_max_{ 1.0 };
+        ::Spacy::Real gamma_min_{ 1.0 };
+    };
+} // namespace Spacy::Preconditioner
