f7f937edb4b978e96250adc96c1d6603d8bd15ff
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-11-08 21:03:47 +0100

    Remove comment
    Change-Id: I2afcc092ff063ba51c91b38614984341fb2e08c6

--- Examples/Kaskade/Tutorial_Spacy/elastomechanics/elastomechanics.hh
+++ Examples/Kaskade/Tutorial_Spacy/elastomechanics/elastomechanics.hh
@@ -1,15 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-/*                                                                           */
-/*  This file is part of the library KASKADE 7                               */
-/*    see http://www.zib.de/projects/kaskade7-finite-element-toolbox         */
-/*                                                                           */
-/*  Copyright (C) 2002-2013 Zuse Institute Berlin                            */
-/*                                                                           */
-/*  KASKADE 7 is distributed under the terms of the ZIB Academic License.    */
-/*    see $KASKADE/academic.txt                                              */
-/*                                                                           */
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 #pragma once
 
 #include <fem/diffops/elastoVariationalFunctionals.hh>
