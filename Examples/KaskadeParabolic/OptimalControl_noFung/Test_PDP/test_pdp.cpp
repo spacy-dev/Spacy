e97f9d540bafb6d49454725a86f08a139019af27
bt702998 <bt702998@btwapmatx02.mat.uni-bayreuth.de>
2021-09-03 12:03:01 +0200

    diverse Änderungen

--- Examples/KaskadeParabolic/OptimalControl_noFung/Test_PDP/test_pdp.cpp
+++ Examples/KaskadeParabolic/OptimalControl_noFung/Test_PDP/test_pdp.cpp
@@ -1,31 +1,20 @@
 #define FUSION_MAX_VECTOR_SIZE 15 // NOLINT(cppcoreguidelines-macro-usage)
 
- 
 #include <chrono>
 #include <iostream>
+#include <io/vtk.hh>
+#include <utilities/kaskopt.hh>
  
 #include <Spacy/Spacy.h>
 #include <Spacy/VectorSpace.h>
 #include <Spacy/Adapter/kaskadeParabolic.hh>
-
 #include <Spacy/Algorithm/PrimalDualProjection/PDP_withoutA.hh>
-#include <Spacy/Algorithm/CG/RegularizeViaPreconditioner.h>
-#include <Spacy/Algorithm/CG/TerminationCriterion.h>
-#include <Spacy/Algorithm/CG/TerminationCriteria.h>
-
-#include <io/vtk.hh>
-#include <utilities/kaskopt.hh>
-
-#include <dune/grid/config.h>
-#include <dune/grid/uggrid.hh>
 
 #include "../optimal_control_withoutfung.hh"
 
-#include "utilities/memory.hh"
-#include "mg/additiveMultigrid.hh"
-
 using std::cout;
 using std::endl;
+using namespace std::chrono;
 using namespace Kaskade;
 using namespace Spacy::KaskadeParabolic;
 
@@ -34,34 +23,32 @@ constexpr int cp = 1;
 
 int main( int argc, char* argv[] )
 {
+    cout << std::setprecision( 10 );
     cout << endl;
+    
     const auto silence = 0;
     std::unique_ptr< boost::property_tree::ptree > pt = getKaskadeOptions( argc, argv, silence, false );
-
+    
+    const auto N                    = getParameter( pt, "stepsInTime", 30 );
+    const auto T                    = getParameter( pt, "endTime", 1. );
+    
+    const auto initialRefinements   = getParameter( pt, "initialRefinements", 5 );
+    const auto iterativeRefinements = getParameter( pt, "iterativeRefinements", 0 );
+    
     const auto maxSteps             = getParameter( pt, "maxSteps", 1000 );
     const auto FEorder              = getParameter( pt, "FEorder", 1 );
     const auto verbose              = getParameter( pt, "verbose", 1 );
     
-    const auto N                    = getParameter( pt, "stepsInTime", 11 );
-    const auto T                    = getParameter( pt, "endTime", 1. );
-    
-    const auto initialRefinements   = getParameter( pt, "initialRefinements", 3 );
-    const auto iterativeRefinements = getParameter( pt, "iterativeRefinements", 0 );
-    
-    double alpha                    = getParameter( pt, "alpha", 0.1 );
+    double alpha                    = getParameter( pt, "alpha", 1e-3 );
     double c                        = getParameter( pt, "cPara", 0 );
     double d                        = getParameter( pt, "dPara", 1 );
     double e                        = getParameter( pt, "ePara", 0 );
-    
-    const auto numberOfCubesX       = getParameter (pt, "NumberOfCubesX", 1);
-    const auto numberOfCubesY       = getParameter (pt, "NumberOfCubesY", 1);
-    const auto numberOfCubesZ       = getParameter (pt, "NumberOfCubesZ", 1);
-    
-    const auto stateAcc             = getParameter (pt, "stateAccuracy", 1e-3);
-    const auto adjointAcc           = getParameter (pt, "adjointAccuracy", 1e-3);
-    const auto modifiedCGAcc        = getParameter (pt, "modifiedCGAccuracy", 1e-3);
-    const auto chebyshevAcc         = getParameter (pt, "chebyshevAccuracy", 1e-3);
-    const auto desiredAccuracy      = getParameter (pt, "desiredAccuracy", 1e-4);
+
+    const auto stateAcc             = getParameter (pt, "stateAccuracy", 1e-1);
+    const auto adjointAcc           = getParameter (pt, "adjointAccuracy", 1e-1);
+    const auto modifiedCGAcc        = getParameter (pt, "modifiedCGAccuracy", 1e-2);
+    const auto chebyshevAcc         = getParameter (pt, "chebyshevAccuracy", 1e-2);
+    const auto desiredAccuracy      = getParameter (pt, "desiredAccuracy", 1e-3);
     
     ///////////////////////////////////////////////// Kaskade FEM-Spaces
     cout << "create FEM-Spaces" << endl;
@@ -95,52 +82,29 @@ int main( int argc, char* argv[] )
     ///////////////////////////////////////////////// Spacy Vector-Spaces
     cout << "create Vector-Spaces" << endl;
     
-    auto totalSpace_ = makeHilbertSpace< VariableDescriptions, Spaces >( gm, "X" );
+    auto totalSpace_ = makeHilbertSpace< Descriptions >( gm, {"y","u","p"}, "X" );
     
-    auto primalSpace_ = makeHilbertSpace< PrimalVariables, Spaces >( gm, "Z" );
+    auto primalSpace_ = makeHilbertSpace< PrimalDescriptions >( gm, {"y","u"}, "Z" );
     using ZXIdxMap = boost::fusion::vector< IdxPair<0,0>, IdxPair<1,1> >;
     setSubSpaceRelation< PrimalDescriptions, Descriptions, ZXIdxMap >( primalSpace_, totalSpace_ );
     
-    auto adjointSpace_ = makeHilbertSpace< DualVariables, Spaces >( gm, "P" );
+    auto adjointSpace_ = makeHilbertSpace< DualDescriptions >( gm, {"p"}, "P" );
     using PXIdxMap = boost::fusion::vector< IdxPair<0,2> >;
     setSubSpaceRelation< DualDescriptions, Descriptions, PXIdxMap >( adjointSpace_, totalSpace_ );
     
-    auto stateSpace_ = makeHilbertSpace< StateVariables, Spaces >( gm, "Y" );
+    auto stateSpace_ = makeHilbertSpace< StateDescriptions >( gm, {"y"}, "Y" );
     using YXIdxMap = boost::fusion::vector< IdxPair<0,0> >;
     setSubSpaceRelation< StateDescriptions, Descriptions, YXIdxMap >( stateSpace_, totalSpace_ );
     using YZIdxMap = boost::fusion::vector< IdxPair<0,0> >;
     setSubSpaceRelation< StateDescriptions, PrimalDescriptions, YZIdxMap >( stateSpace_, primalSpace_ );
     
-    auto controlSpace_ = makeHilbertSpace< ControlVariables, Spaces >( gm, "U" );
+    auto controlSpace_ = makeHilbertSpace< ControlDescriptions >( gm, {"u"}, "U" );
     using UXIdxMap = boost::fusion::vector< IdxPair<0,1> >;
     setSubSpaceRelation< ControlDescriptions, Descriptions, UXIdxMap >( controlSpace_, totalSpace_ );
     using UZIdxMap = boost::fusion::vector< IdxPair<0,1> >;
     setSubSpaceRelation< ControlDescriptions, PrimalDescriptions, UZIdxMap >( controlSpace_, primalSpace_ );
     
     ///////////////////////////////////////////////// Functional 
-    auto resultRef = zero( stateSpace_ );
-    for (int t = 0; t < N; t++)
-    {
-        auto& resultRefK = Spacy::cast_ref< Vector< StateDescriptions > >( resultRef );
-
-        const Dune::FieldVector< double, 1 > reference_deformationVector{1};
-        auto& refImpl = boost::fusion::at_c< 0 >( resultRefK.get_nonconst(t).data ).coefficients();
-        for ( int i = 0; i < refImpl.size(); ++i )
-            refImpl[ i ] = reference_deformationVector;
-    }
-    
-    auto resultRefP = zero( adjointSpace_ );
-    for (int t = 0; t < N; t++)
-    {
-        auto& resultRefKP = Spacy::cast_ref< Vector< DualDescriptions > >( resultRefP );
-
-        const Dune::FieldVector< double, 1 > reference_deformationVector{1};
-        auto& refImpl = boost::fusion::at_c< 0 >( resultRefKP.get_nonconst(t).data ).coefficients();
-        for ( int i = 0; i < refImpl.size(); ++i )
-            refImpl[ i ] = reference_deformationVector;
-    }
-    
-    
     cout << "create Functional" << endl;
     
     using FunctionalDefinition = TangentialStepFunctional< 0, 1, 2, double, Descriptions >;
@@ -167,20 +131,18 @@ int main( int argc, char* argv[] )
     auto M                  = makeSymmetricCallableBlock<FunctionalDefinition>(fL,primalSpace_);
     auto Mu                 = makeSymmetricCallableBlock<FunctionalDefinition>(fL,controlSpace_);
     
-    auto D                  = makeMassMatrixBlock<FunctionalDefinition>(fL,stateSpace_,adjointSpace_);
-    auto DT                 = makeMassMatrixBlock<FunctionalDefinition>(fL,adjointSpace_,stateSpace_);
+    auto D                  = makeMassMatrixBlock<StateDescriptions,DualDescriptions>(fL,stateSpace_,adjointSpace_);
+    auto DT                 = makeMassMatrixBlock<DualDescriptions,StateDescriptions>(fL,adjointSpace_,stateSpace_);
 
-    auto BPX                = makeSingleBlockPreconditioner<DualDescriptions,StateDescriptions,FunctionalDefinition>
-                                (adjointSpace_,stateSpace_,fL );
-    auto BPXT               = makeSingleBlockPreconditioner<StateDescriptions,DualDescriptions,FunctionalDefinition>
-                                (stateSpace_,adjointSpace_,fL );
-         
+    auto Prec                = makeSingleBlockPreconditioner<DualDescriptions,StateDescriptions,FunctionalDefinition>(adjointSpace_,stateSpace_,fL );
+    auto PrecT               = makeSingleBlockPreconditioner<StateDescriptions,DualDescriptions,FunctionalDefinition>(stateSpace_,adjointSpace_,fL,true );
+                                
     auto controlSolver_     = makeDirectSolver<ControlDescriptions,ControlDescriptions>(Mu,gm,controlSpace_, controlSpace_);
-    auto stateSolver_       = makePDESolver<StateDescriptions,DualDescriptions>(D,A,BPX,gm.getTempGrid().getDtVec());
-    auto adjointSolver_     = makePDESolver<DualDescriptions,StateDescriptions>(DT,AT,BPXT,gm.getTempGrid().getDtVec(),false);
-    auto surrStateSolver_   = makeSurrogatePDESolver<StateDescriptions,DualDescriptions>(D,BPX,A,gm.getTempGrid().getDtVec());
-    auto surrAdjointSolver_ = makeSurrogatePDESolver<DualDescriptions,StateDescriptions>(DT,BPXT,AT,gm.getTempGrid().getDtVec(),false);
-    
+    auto stateSolver_       = makePDESolver(D,A,Prec,gm.getTempGrid().getDtVec(),gm,stateAcc);
+    auto adjointSolver_     = makePDESolver(DT,AT,PrecT,gm.getTempGrid().getDtVec(),gm,adjointAcc,false);
+    auto surrStateSolver_   = makeSurrogatePDESolver<StateDescriptions,DualDescriptions>(A,Prec,gm.getTempGrid().getDtVec());
+    auto surrAdjointSolver_ = makeSurrogatePDESolver<DualDescriptions,StateDescriptions>(AT,PrecT,gm.getTempGrid().getDtVec(),false);
+
     ///////////////////////////////////////////////// Execute
     cout << "Execute" << endl;
 
@@ -190,17 +152,14 @@ int main( int argc, char* argv[] )
     pdp.setRelativeAccuracy(desiredAccuracy);
     pdp.setInternalAccuracies(stateAcc,adjointAcc,modifiedCGAcc,chebyshevAcc);
     
-    cout << "StateSolver:" << endl;
-    stateSolver_(resultRefP);
-    cout << "AdjointSolver:" << endl;
-    adjointSolver_(resultRef);
-    cout << "StateSolver(Surr):" << endl;
-    surrStateSolver_(resultRefP);
-    cout << "AdjointSolver(Surr):" << endl;
-    surrAdjointSolver_(resultRef);
-    cout << "done" << endl;
     
-    auto res = totalSpace_.embed( pdp(b) );
+    auto startTime = high_resolution_clock::now();
+    
+    auto res = totalSpace_.embed( pdp(b) );                         // Start PDP
+    
+    auto endTime = high_resolution_clock::now() - startTime;
+    cout << "computation time: " << duration_cast< seconds >( endTime ).count() << "s, " 
+            << duration_cast< milliseconds >( endTime ).count() % 1000 << "ms." << endl << endl;
     
     ///////////////////////////////////////////////// Results
     cout << "finishing ... " << std::flush;
